76298707a3d6da57b272b354a092a6f8b1c30ed5
BlackyCGS <glebsapega0307@gmail.com>
2025-03-17 22:30:52 +0300

    Delete unused methods

--- src/main/java/com/myshop/internetshop/classes/dto/OrderDto.java
+++ src/main/java/com/myshop/internetshop/classes/dto/OrderDto.java
@@ -25,18 +25,4 @@ public class OrderDto {
         this.userId = order.getUser().getUserId();
         this.products = order.getProducts();
     }
-
-    OrderDto(int orderId, int orderStatus, int userId, List<Product> products) {
-        this.orderId = orderId;
-        this.orderStatus = orderStatus;
-        this.userId = userId;
-        this.products = products;
-    }
-
-    public Order toOrder() {
-        Order order = new Order();
-        order.setOrderStatus(orderStatus);
-        order.setProducts(products);
-        return order;
-    }
 }
